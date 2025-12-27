158f0e2b4767a6810b585863a84d37c34e622df9
JavierZavala17 <javier_z_t@hotmail.com>
2025-12-26 19:20:12 -0600

    Add verbose option

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 # Load GEMINI_API_KEY
 load_dotenv()
@@ -13,18 +14,23 @@ if not api_key:
 # Accept user input
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-# Now we can access `args.user_prompt`
+# Activate verbose responses
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()  # Now we can access `args.user_prompt`
+
+# Array that will contain the conversation with GEMINI
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 # Initiate call to GEMINI
 client = genai.Client(api_key=api_key)
-model = "gemini-2.5-flash"
-response = client.models.generate_content(model = model, contents = args.user_prompt)
+response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages)
 
 if not response.usage_metadata:
     raise RuntimeError("USAGE_METADATA IS NONE")
 
-print(f"User prompt: {args.user_prompt}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 print(f"Responses:\n{response.text}")
