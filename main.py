f11a9926ffe89538fea6716753eaa1a1530ac4e5
JavierZavala17 <javier_z_t@hotmail.com>
2025-12-26 18:22:36 -0600

    Check tokens

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ if not api_key:
     raise RuntimeError("GEMINI_API_KEY NOT FOUND")
 
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model = "gemini-2.5-flash", 
-    contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+model = "gemini-2.5-flash"
+contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+response = client.models.generate_content(model = model, contents = contents)
 
-print(response.text)
+if not response.usage_metadata:
+    raise RuntimeError("USAGE_METADATA IS NONE")
+
+print(f"User prompt: {contents}")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Responses:\n{response.text}")
