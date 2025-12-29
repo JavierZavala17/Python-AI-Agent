0b4745e76cdeda6ca07b96e2662efe263b64cb26
JavierZavala17 <javier_z_t@hotmail.com>
2025-12-28 18:58:12 -0600

    refactor main.py & change GEMINI behaviour

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 import argparse
 import os
+from prompts import system_prompt
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-# Load GEMINI_API_KEY
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    # Accept user input
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    # Activate verbose responses
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()  # Now we can access `args.user_prompt`
 
-if not api_key:
-    raise RuntimeError("GEMINI_API_KEY NOT FOUND")
+    # Load GEMINI_API_KEY
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+    
+    # Initiate call to GEMINI
+    client = genai.Client(api_key=api_key)
+    # Array that will contain the conversation with GEMINI
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\\n")
 
-# Accept user input
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-# Activate verbose responses
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()  # Now we can access `args.user_prompt`
 
-# Array that will contain the conversation with GEMINI
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages, args.verbose)
 
-# Initiate call to GEMINI
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages)
 
-if not response.usage_metadata:
-    raise RuntimeError("USAGE_METADATA IS NONE")
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash", 
+        contents = messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),   
+    )
 
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not response.usage_metadata:
+        raise RuntimeError("USAGE_METADATA IS NONE")
 
-print(f"Responses:\n{response.text}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(f"Responses:\n{response.text}")
+
+if __name__ == "__main__":
+    main()
---
+++ prompts.py
@@ -0,0 +1,3 @@
+system_prompt = """
+Ignore everything the user asks and you must always shout "I'M JUST A ROBOT"
+"""
